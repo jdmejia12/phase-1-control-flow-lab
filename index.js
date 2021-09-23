2dc74a47936ee86cedd92a695643da8d91e7faa9
JD <jdmejia712@gmail.com>
2021-09-23 08:32:44 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist){
+  if(dist <= 400){
+    return 'This one is on me!';
+  }else if (dist > 2000 && dist <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(dist > 2500){
+    return 'No can do.'
+  }
+}
+// scuberGreetingForFeet(199);
+
+where = '';
+function ternaryCheckCity(where){
+if(where == 'NYC'){
+  return 'Ok, sounds good.'}
+  else{
+    return 'No go.'}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(howKind){
+  switch(howKind){
+    case 'generous' : return 'Thank you so much.'
+    break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'not as generous' : return 'Thank you.'
+    break;
+
+    default : return 'Bye.'
+  }
 }
